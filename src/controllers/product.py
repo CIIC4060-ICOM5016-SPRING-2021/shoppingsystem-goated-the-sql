7f960062dc12536db08154f07f18d8c2350cd98b
Francisco Casiano <francisco.casiano2@upr.edu>
2022-03-28 17:53:26 -0400

    Simplified Product Controller code

--- src/controllers/product.py
+++ src/controllers/product.py
@@ -15,16 +15,10 @@ class ProductController:
         # TODO: Make this usable by a web browser
         return ProductModel().get_product(prod_id)
 
-    def change_visibility(self, prod_id, user_id, visibility: bool):
-        # TODO: Move this check to the models section?
-        if UserModel().get_user(user_id).db_is_admin():
-            product = ProductModel().get_product(prod_id)
-            product.set_visibility(visibility)
-            ProductModel().update_product(prod_id, product)
+    @classmethod
+    def change_visibility(cls, prod_id, user_id, visibility: bool):
+        ProductModel().db_set_visibility(user_id, prod_id, visibility)
 
-
-    def change_price(self, prod_id, user_id, new_price):
-        # TODO: Move this check to the models section?
-        if UserModel().get_user(user_id).db_is_admin():
-            product = ProductModel().get_product(prod_id)
-            product.set_price(new_price)
+    @classmethod
+    def change_price(cls, prod_id, user_id, new_price):
+        ProductModel().db_set_price(user_id, prod_id, new_price)
