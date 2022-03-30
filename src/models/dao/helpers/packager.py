4b1b00e9caffc66676c09231dad080b6d8963f8e
Francisco Casiano <francisco.casiano2@upr.edu>
2022-03-30 17:59:23 -0400

    Added documentation to the Packager class

--- src/models/dao/helpers/packager.py
+++ src/models/dao/helpers/packager.py
@@ -3,19 +3,33 @@
 class Packager:
     @classmethod
     def package_response(cls, response, obj_type):
+        """
+            Converts a response from the database into an object related to its entity.
+
+        :param response: List[Tuple] or Tuple containing the information of the entity model desired
+        :param obj_type: Entity Model desired
+        :return: List[Entity Model] if response is a List or Entity Model if response is a single Tuple
+        """
         if response is None:
             return None
         elif type(response) == list:
             result = list()
 
             for item in response:
-                result.append(cls.convert(item, obj_type))
+                result.append(cls.convert_tuple_to_obj(item, obj_type))
             return result
         else:
-            return cls.convert(response, obj_type)
+            return cls.convert_tuple_to_obj(response, obj_type)
 
     @classmethod
-    def convert(cls, item: tuple, to_obj):
+    def convert_tuple_to_obj(cls, item: tuple, to_obj):
+        """
+            Converts a given Tuple into the desired Entity Model Object
+
+        :param item: Tuple to convert
+        :param to_obj: String containing the name of Entity Model Object desired
+        :return: Entity Model, same type as requested
+        """
         if to_obj == 'UserModel':
             from src.models.user import UserModel
             result = UserModel()
