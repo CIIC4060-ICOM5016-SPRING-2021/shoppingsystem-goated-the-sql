c5f010e7d91cb65d585ef9580e5e8c0eb3b93d43
Francisco Casiano <francisco.casiano2@upr.edu>
2022-03-30 17:39:04 -0400

    Added documentation to the Product Controller

--- src/controllers/product.py
+++ src/controllers/product.py
@@ -1,12 +1,16 @@
 from flask import jsonify
 
 from src.models.product import ProductModel
-from src.models.user import UserModel
 
 
 class ProductController:
     @classmethod
     def get_all_products(cls):
+        """
+            Prompts the database to get the full catalog of products
+
+        :return: list of JSONs containing all the products in the database
+        """
         products = []
         for product in ProductModel().get_all_products():
             products.append(ProductController().preparer(product))
@@ -14,6 +18,12 @@ class ProductController:
 
     @classmethod
     def get_product(cls, prod_id):
+        """
+            Prompts the database to search for a product
+
+        :param prod_id: id of the product being searched for
+        :return: JSON containing the queried product, 404 error if not found
+        """
         queried_product = ProductModel().get_product(prod_id)
 
         if not queried_product:
@@ -23,14 +33,34 @@ class ProductController:
 
     @classmethod
     def change_visibility(cls, prod_id, user_id, visibility: bool):
+        """
+            Prompts the database to make a visibility parameter change to a product.
+
+        :param prod_id: id of the product to be altered
+        :param user_id: id of the user requesting the change
+        :param visibility: updated visibility state of the product
+        """
         ProductModel().db_change_visibility(user_id, prod_id, visibility)
 
     @classmethod
     def change_price(cls, prod_id, user_id, new_price):
+        """
+            Prompts the database to make a price change to a product.
+
+        :param prod_id: id of the product to be altered
+        :param user_id: id of the user requesting the change
+        :param new_price: updated price of the product
+        """
         ProductModel().db_change_price(user_id, prod_id, new_price)
 
     @classmethod
     def preparer(cls, product):
+        """
+            Creates a python dictionary equivalent of a given Product Model
+
+        :param product: ProductModel to convert
+        :return: dictionary equivalent of the ProductModel given
+        """
         prodict = {
             'id': product.get_prod_id(),
             'name': product.get_name(),
