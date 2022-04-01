f6554c88498d750fae15645e90a13314248f1d6e
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-04-01 16:03:04 -0400

    Re-added documentation to backend.py lost in commit e158430c0

--- src/models/dao/backend.py
+++ src/models/dao/backend.py
@@ -5,7 +5,12 @@ from src.models.dao.helpers.packager import Packager
 class BackEnd:
     @classmethod
     def create_element(cls, model):
-        # Registers a user
+        """
+           Creates a new element within the corresponding Entity Model table in the database
+
+        :param model: Entity Model
+        :return: A copy of the information added to the database (same Entity Model as the model given)
+        """
         if model.__class__.__name__ == 'UserModel':
             return cls.__db_fetch_one(
                 """
@@ -44,6 +49,14 @@ class BackEnd:
 
     @classmethod
     def get_element(cls, model, pk, select_attributes: str):
+        """
+            Queries the corresponding entity table in the database for the given primary key.
+
+        :param model: class instance of the desired Entity Model
+        :param pk: primary key corresponding to the Entity Model
+        :param select_attributes: attributes to retrieve from the database
+        :return: Desired Entity Model with the information corresponding to the primary key queried
+        """
         if model.__class__.__name__ == 'UserModel':
             # If the where_clause_statement is not empty
             # TODO: What happens if there are no results?
@@ -78,6 +91,15 @@ class BackEnd:
 
     @classmethod
     def delete_element(cls, model, pk):
+        """
+            Completely removes a row from the corresponding table.
+
+        :param model: class instance of the desired Entity Model
+        :param pk: primary key corresponding to the Entity Model
+        :return: boolean if the deletion was successfully completed
+        """
+
+        # TODO: Make the function return the boolean on completion
         if model.__class__.__name__ == 'UserModel':
             cls.__db_run_command(
                 """
@@ -101,6 +123,14 @@ class BackEnd:
 
     @classmethod
     def get_all_elements(cls, model, select_attributes: str, filter_clause: str):
+        """
+            Queries the database for all the elements of the given corresponding Entity.
+
+        :param model: class instance of the desired Entity Model
+        :param select_attributes: attributes desired from the query
+        :param filter_clause: specific filters desired for the query
+        :return: list containing the desired Entity Models of matching query results
+        """
         if model.__class__.__name__ == 'ProductModel':
 
             # If the where_clause_statement is not empty
@@ -153,6 +183,16 @@ class BackEnd:
     @classmethod
     def get_all_elements_ordered(
             cls, model, select_attributes: str, filter_clause: str, order_attribute: str, sort: str):
+        """
+            Queries the database for all the elements of the given corresponding Entity in a specified order.
+
+        :param model: class instance of the desired Entity Model
+        :param select_attributes: attributes desired from the query
+        :param filter_clause: specific filters desired for the query
+        :param order_attribute: specific attribute to order
+        :param sort: ascending (ASC) or descending (DESC)
+        :return: list containing the desired Entity Models of matching query results in the specified order
+        """
         if model.__class__.__name__ == 'ProductModel':
             # If the where_clause_statement is not empty (ie no filter required)
             if filter_clause:
@@ -186,6 +226,13 @@ class BackEnd:
 
     @classmethod
     def update_element_attribute(cls, table: str, change: str, filter_clause: str):
+        """
+            Updates a specific attribute in a given table.
+
+        :param table: table where
+        :param change: specific change to attribute
+        :param filter_clause: specific filters for desired change
+        """
         # Registers a user
         if table.lower() == 'usr':
             cls.__db_run_command(
@@ -231,6 +278,13 @@ class BackEnd:
 
     @classmethod
     def __db_fetch_one(cls, command: str, return_type: str):
+        """
+            Backend private function. Runs a query that returns a single result.
+
+        :param command: command to be run in database
+        :param return_type: type of entity model desired (String)
+        :return: desired Entity Model of matching query result
+        """
         db_connection = DBAccess().connect_to_db()
         cursor = db_connection.cursor()
 
@@ -246,6 +300,13 @@ class BackEnd:
 
     @classmethod
     def __db_fetch_all(cls, command: str, return_type: str):
+        """
+            Backend private function. Runs a query that returns multiple results.
+
+        :param command: command to be run in database
+        :param return_type: type of entity model desired (String)
+        :return: list containing the desired Entity Models of matching query results
+        """
         db_connection = DBAccess().connect_to_db()
         cursor = db_connection.cursor()
 
@@ -259,6 +320,11 @@ class BackEnd:
 
     @classmethod
     def __db_run_command(cls, command: str):
+        """
+            Backend private function. Runs a custom query in the database.
+
+        :param command: command to be run in database
+        """
         db_connection = DBAccess().connect_to_db()
         cursor = db_connection.cursor()
 
