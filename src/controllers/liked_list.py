237ca03e11ac881077782d3c62a58d3ba087396b
Ivan Jackson <ivan.jackson@upr.edu>
2022-04-09 10:21:10 -0400

    Deleted useless comments and unused imports

--- src/controllers/liked_list.py
+++ src/controllers/liked_list.py
@@ -1,14 +1,10 @@
 from flask import jsonify
-
-from src.models.liked_list import LikedListModel
-from src.models.dao.backend import BackEnd
 from src.models.liked_list import LikedListModel
 
 
 class LikedListController:
     @classmethod
     def get_likes(cls, user_id):
-        # TODO: Implement calls to logic in models package
         likes_list = len(LikedListModel().get_all_elements(user_id))
         return likes_list
 
