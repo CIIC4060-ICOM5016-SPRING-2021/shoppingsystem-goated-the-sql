7187ddb1ef1c7f8e112f664969572505010f9907
ed <edwinvegareyes@gmail.com>
2022-05-12 13:03:44 -0400

    Removing demo tab, adding top navigation bar.

--- src/frontend/shop-frontend/src/UserView.js
+++ src/frontend/shop-frontend/src/UserView.js
@@ -1,12 +1,13 @@
 import React, {Component, useState} from 'react';
-import {Button, Card, Container, Divider, Header, Modal, Tab} from "semantic-ui-react";
+import {Button, Card, Container, Divider, Header, Input, Menu, Modal, Tab} from "semantic-ui-react";
 import Dashboard from "./Dashboard";
 import Products from "./Products";
 
 
 function UserView(){
-    const [isAuth, setIsAuth] = useState(true)
+/*    const [isAuth, setIsAuth] = useState(true)
     const [notShow, setNotShow] = useState(false)
+
     const panes = [
         {
             menuItem: 'Products', render: () => <Tab.Pane active={isAuth}><Container><Header>Anything you need to put here</Header><Divider/></Container><Products/></Tab.Pane>
@@ -25,7 +26,24 @@ function UserView(){
         }
     ]
 
-    return <Tab panes={panes}/>
+    return <Tab panes={panes}/>*/
+
+    //const [isActive, setIsActive] = useState( '')
+    //const setActive = (tab) => setIsActive( tab);
+    const items = [
+        { key: 'Dashboard', active: true, name: 'Dashboard' },
+        { key: 'Products', name: 'Products' ,position:'right'},
+        { key: 'LikedList', name: 'Likes' },
+        { key: 'Cart', name: 'Cart'},
+        { key: 'Orders', name: 'Orders' },
+        { key: 'LogOut', name: 'Log Out' },
+    ]
+
+    return (
+        <div>
+        <Menu items={items}></Menu>
+        </div>
+    )
 
 }
 export default UserView;
