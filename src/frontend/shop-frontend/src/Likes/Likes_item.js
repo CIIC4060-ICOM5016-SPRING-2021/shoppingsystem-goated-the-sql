0eb510afe69c43442a76a5d95d8eb886efb4ab6f
Francisco Casiano <personal@franciscocasiano.com>
2022-05-19 19:26:56 -0600

    You can now view items directly from cart

--- src/frontend/shop-frontend/src/Likes/Likes_item.js
+++ src/frontend/shop-frontend/src/Likes/Likes_item.js
@@ -1,48 +1,48 @@
-import React, {Component, useState} from 'react';
-import {Button, Card, CardContent, Container, Icon, Image, Label, Tab} from "semantic-ui-react";
-import {Link} from "react-router-dom";
+import React, { Component, useState } from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Icon,
+  Image,
+  Label,
+  Tab,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import Product from "../Product/Product";
 
 import rtx from "../3080.png";
 
-
-
-
-function Likes_item(props){
-    return props.info.map(value => {
-        var product = {
-            id: value.id.toString(),
-            name: value.name.toString(),
-            price: value.price.toString(),
-            desc: value.desc.toString(),
-            category: value.category.toString()
-
-        }
-        return <Card >
-            <Image src={rtx} wrapped ui={false}/>
-            <Card.Content className={"text"}>
-                <h3 className={"text"}>{product.name}</h3>
-                <h3 className={"text"}>${product.price}</h3>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui three buttons'>
-                    <Button
-                        as={Link}
-                        to={product.id}
-                        color ='red'
-                        icon>
-                        <Icon name = "like"></Icon>
-                    </Button>
-                    <Button  color={"blue"}>
-                        View
-                    </Button>
-                    <Button   color={"green"}>
-                        Add
-                    </Button>
-                </div>
-            </Card.Content>
-        </Card>
-
-            });
+function Likes_item(props) {
+  return props.info.map((value) => {
+    var product = {
+      id: value.id.toString(),
+      name: value.name.toString(),
+      price: value.price.toString(),
+      desc: value.desc.toString(),
+      category: value.category.toString(),
+    };
+    return (
+      <Card>
+        <Image src={rtx} wrapped ui={false} />
+        <Card.Content className={"text"}>
+          <h3 className={"text"}>{product.name}</h3>
+          <h3 className={"text"}>${product.price}</h3>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="ui three buttons">
+            <Button as={Link} to={product.id} color="red" icon>
+              <Icon name="like"></Icon>
+            </Button>
+            <Button as={Link} to={`/Products/${product.id}`} color={"blue"}>
+              View
+            </Button>
+            <Button color={"green"}>Add</Button>
+          </div>
+        </Card.Content>
+      </Card>
+    );
+  });
 }
 export default Likes_item;
