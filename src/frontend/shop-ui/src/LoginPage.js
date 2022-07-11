1652f10c2c3dfaae7bd681f29077338584039cb3
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-07-10 20:13:48 -0600

    Centered login page!

--- src/frontend/shop-ui/src/LoginPage.js
+++ src/frontend/shop-ui/src/LoginPage.js
@@ -7,18 +7,24 @@ function LoginPage() {
   return (
     //TODO: Figure out a way to center this on the page
     <>
-      <Container>
-        <Segment placeholder raised>
-          <h1>Welcome to the Goat Store</h1>
-          <Form align="left">
-            <Form.Input label="First Name" />
-            <Form.Input label="Password" type="password" />
-          </Form>
-          <Button as={Link} to="/home" primary content="Log In" />
-          <p>Don't have an account?</p>
-          <Link to="/sign-up">Sign Up</Link>
-        </Segment>
-      </Container>
+      <div className="login-page-body">
+        <Container>
+          <Segment secondary raised>
+            <h1>Welcome to the Goat Store</h1>
+
+            <Form align="left" size="large" className="form-input-boxes">
+              <Form.Group grouped>
+                <Form.Input label="First Name" />
+                <Form.Input label="Password" type="password" />
+              </Form.Group>
+            </Form>
+
+            <Button as={Link} to="/home" primary content="Log In" />
+            <p>Don't have an account?</p>
+            <Link to="/sign-up">Sign Up</Link>
+          </Segment>
+        </Container>
+      </div>
     </>
   );
 }
