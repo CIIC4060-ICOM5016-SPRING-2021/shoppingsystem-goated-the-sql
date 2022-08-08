328ebecbb160077b4640219ea1a00aa8d32ed2e1
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-08-08 14:56:18 -0600

    Most bought user stat completed

--- src/frontend/shop-ui/src/components/stats-page/personal/most-bought.js
+++ src/frontend/shop-ui/src/components/stats-page/personal/most-bought.js
@@ -1,5 +1,56 @@
-function MostBought() {
-    return ( <>goomba 4</> );
+import {Grid, List, ListItem} from "semantic-ui-react";
+import {Bar, BarChart, Cell, XAxis, YAxis} from "recharts";
+
+function MostBought(props) {
+    const accountStats = props.details["User Statistics"]
+
+
+    return (
+      <>
+          <div>
+              <Grid>
+                  <div className="hottest-stats-categories">
+                      <Grid.Row>
+                          <h3>Most Bought Categories</h3>
+                          {/* TODO: Add CSS for the following */}
+                          <div className="hottest-stats-categories-inside">
+                              <BarChart width={335} height={150} data={accountStats[0]["Most Bought Categories"]}>
+                                  <XAxis dataKey="name"/>
+                                  <YAxis/>
+                                  <Bar dataKey="quantity_bought">
+
+                                      {accountStats[0]["Most Bought Categories"].map((category, index) => (
+                                        <Cell
+                                          cursor="pointer"
+                                          fill="#0984e3"
+                                          key={`cell-${index}`}
+                                        />
+                                      ))}
+                                  </Bar>
+                              </BarChart>
+                          </div>
+                      </Grid.Row>
+                  </div>
+                  <div className="hottest-stats-products">
+                      <Grid.Row>
+                          <h3>Most Bought Products</h3>
+                          {/* TODO: Add CSS for the following */}
+                          <div className="hottest-stats-products-inside">
+                              <List ordered>
+                                  {accountStats[1]["Most Bought Products"].map((product, index) => (
+                                    <ListItem
+                                      content={product.name}
+                                      key={`list-item-${index}`}
+                                    />
+                                  ))}
+                              </List>
+                          </div>
+                      </Grid.Row>
+                  </div>
+              </Grid>
+          </div>
+      </>
+    );
 }
 
 export default MostBought;
