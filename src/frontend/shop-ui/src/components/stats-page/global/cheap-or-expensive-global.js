b16194d5757dc4de1cc3ff549bcd6b754063fe33
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-08-08 13:59:58 -0600

    Cheapest and Most Expensive Product Stats complete for Global

--- src/frontend/shop-ui/src/components/stats-page/global/cheap-or-expensive-global.js
+++ src/frontend/shop-ui/src/components/stats-page/global/cheap-or-expensive-global.js
@@ -1,8 +1,5 @@
-import { Grid } from "semantic-ui-react";
-import {
-  Funnel,
-  FunnelChart,
-} from "recharts";
+import {Grid} from "semantic-ui-react";
+import {Funnel, FunnelChart, LabelList, Tooltip,} from "recharts";
 
 function CheapnExpensiveProductsList(props) {
   const globalStats = props.details["Global Statistics"]
@@ -18,12 +15,23 @@ function CheapnExpensiveProductsList(props) {
               <h3>Cheapest Products</h3>
               {/* TODO: Add CSS for the following */}
               <div className="cheapest-stats-inside">
-                <FunnelChart
-                  width={335}
-                  height={150}
-                  data={globalStats["Cheapest Products"]}
-                >
-                  <Funnel dataKey="price"></Funnel>
+                <FunnelChart width={335} height={250} >
+                  <Tooltip/>
+                  <Funnel dataKey="price" data={globalStats["Cheapest Products"]} isAnimationActive>
+                    <LabelList position="center" fill="#000" stroke="none" dataKey="name" />
+                  </Funnel>
+                </FunnelChart>
+              </div>
+            </Grid.Row>
+            <Grid.Row>
+              <h3>Most Expensive Products</h3>
+              {/* TODO: Add CSS for the following */}
+              <div className="cheapest-stats-inside">
+                <FunnelChart width={335} height={250} >
+                  <Tooltip/>
+                  <Funnel dataKey="price" data={globalStats["Most Expensive Products"]} isAnimationActive>
+                    <LabelList position="center" fill="#000" stroke="none" dataKey="name" />
+                  </Funnel>
                 </FunnelChart>
               </div>
             </Grid.Row>
