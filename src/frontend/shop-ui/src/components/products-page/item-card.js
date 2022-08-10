ab2ed2c676b3626668000e4aea0f9f25191204aa
Ivan Jackson <ivan.jackson@upr.edu>
2022-08-10 09:26:00 -0400

    Merged with upstream, added the filtering and liking

--- src/frontend/shop-ui/src/components/products-page/item-card.js
+++ src/frontend/shop-ui/src/components/products-page/item-card.js
@@ -8,204 +8,159 @@ import {addLikedItem} from "../../features/likes/likesSlice";
 import {filterByCat, orderByPriceAsc, orderByPriceDesc} from "../../features/products/productSlice";
 
 function Products() {
-  const {products, isLoading} = useSelector((store) => store.product);
-  const dispatch = useDispatch();
+    const {products, isLoading} = useSelector((store) => store.product);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getAllProducts())
-    }, [])
+    }, []);
+
+
     const categories = [
         {
             key: 0,
+            text: "All",
+            value: "all",
+        },
+        {
+            key: 1,
             text: "Audio",
             value: "audio",
         },
         {
-            key: 1,
+            key: 2,
             text: "Collectibles",
             value: "collectibles",
         },
         {
-            key: 2,
+            key: 3,
             text: "Consoles",
             value: "consoles",
         },
         {
-            key: 3,
+            key: 4,
             text: "Controllers",
             value: "controllers",
         },
         {
-            key: 4,
+            key: 5,
             text: "Gift Cards",
             value: "gift cards",
         },
         {
-            key: 5,
+            key: 6,
             text: "PC",
             value: "pc",
         },
         {
-            key: 6,
+            key: 7,
             text: "Peripherals",
             value: "peripherals",
         },
         {
-            key: 7,
+            key: 8,
             text: "Smartphones",
             value: "smartphones",
         },
         {
-            key: 8,
+            key: 9,
             text: "Storage",
             value: "storage",
         },
         {
-            key: 9,
+            key: 10,
             text: "Video Games",
             value: "video games",
         },
     ];
-  const categories = [
-    {
-      key: 0,
-      text: "All",
-      value: "all",
-    },
-    {
-      key: 1,
-      text: "Audio",
-      value: "audio",
-    },
-    {
-      key: 2,
-      text: "Collectibles",
-      value: "collectibles",
-    },
-    {
-      key: 3,
-      text: "Consoles",
-      value: "consoles",
-    },
-    {
-      key: 4,
-      text: "Controllers",
-      value: "controllers",
-    },
-    {
-      key: 5,
-      text: "Gift Cards",
-      value: "gift cards",
-    },
-    {
-      key: 6,
-      text: "PC",
-      value: "pc",
-    },
-    {
-      key: 7,
-      text: "Peripherals",
-      value: "peripherals",
-    },
-    {
-      key: 8,
-      text: "Smartphones",
-      value: "smartphones",
-    },
-    {
-      key: 9,
-      text: "Storage",
-      value: "storage",
-    },
-    {
-      key: 10,
-      text: "Video Games",
-      value: "video games",
-    },
-  ];
 
-  function likeItem(id) {
-    dispatch(addLikedItem(id));
-  }
-  function orderByPriceAscending() {
-    dispatch(orderByPriceAsc());
-  }
-  function orderByPriceDescending() {
-    dispatch(orderByPriceDesc());
-  }
-  function orderByCategory(e,category) {
-    dispatch(filterByCat(category));
-  }
+    function likeItem(id) {
+        dispatch(addLikedItem(id));
+    }
 
-  if (isLoading) {
-    return <Loading/>
-  } else {
-    return (
-      <>
-        <div className="product-list-body">
-          <Grid celled="internally">
-            <Grid.Row>
-              <div className="product-list-sort-selection">
-                <h1>Sort by</h1>
-                {/* TODO: Add indicator of which button was clicked */}
-                {/* TODO: Disable category button if filter by price has been selected and vice versa */}
-                <Button icon="up arrow" basic content="Price: Ascending" onClick={() => orderByPriceAscending()}/>
-                <Button icon="down arrow" basic content="Price: Descending" onClick={() => orderByPriceDescending()}/>
-                <Dropdown
-                  placeholder="Category"
-                  selection
-                  options={categories}
-                  defaultValue="all"
-                  onChange={orderByCategory}
+    function orderByPriceAscending() {
+        dispatch(orderByPriceAsc());
+    }
 
-                  // TODO: Find out how to add onClick functionality to dynamic selection values
-                ></Dropdown>
-              </div>
-            </Grid.Row>
-            <Grid.Row>
-              <div className="product-list-item-cards">
-                <Card.Group itemsPerRow={3}>
-                  {products["Products"].map((item) => (
-                    <Card key={item.id}>
-                      <Image
-                        src="https://i.ytimg.com/vi/z_wcL_zg2hM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDo4Dzvudez6Op0QRM9DCt9INaCZQ"/>
-                      <Card.Content>
-                        <Card.Header content={item.name}/>
-                        <Card.Meta content={item.category}/>
-                        <Card.Description content={item.desc}/>
-                      </Card.Content>
-                      <Card.Content>
-                        <div className="item-card-body">
-                          <div className="price-n-quantity">
-                            <div className="item-card-price">${item.price}</div>
-                            <Button.Group basic size="medium">
-                              <Button icon="plus"/>
-                              <Button disabled content="14"/>
-                              <Button icon="minus"/>
-                            </Button.Group>
-                          </div>
-                          <div className="item-card-cart-n-wishlist">
-                            <Button icon="heart" primary onClick={() => likeItem(item.id)}/>
-                            <Button positive animated="fade" size="medium" fluid>
-                              <Button.Content visible>
-                                <Icon name="arrow right"/>
-                              </Button.Content>
-                              <Button.Content hidden>Buy</Button.Content>
-                            </Button>
-                          </div>
-                        </div>
-                      </Card.Content>
-                    </Card>
-                  ))}
-                </Card.Group>
-              </div>
-            </Grid.Row>
-          </Grid>
-          {/*</React.Suspense>*/}
-        </div>
-      </>
-    );
-  }
+    function orderByPriceDescending() {
+        dispatch(orderByPriceDesc());
+    }
+
+    function orderByCategory(e, category) {
+        dispatch(filterByCat(category));
+    }
+
+    if (isLoading) {
+        return <Loading/>
+    } else {
+        return (
+            <>
+                <div className="product-list-body">
+                    <Grid celled="internally">
+                        <Grid.Row>
+                            <div className="product-list-sort-selection">
+                                <h1>Sort by</h1>
+                                {/* TODO: Add indicator of which button was clicked */}
+                                {/* TODO: Disable category button if filter by price has been selected and vice versa */}
+                                <Button icon="up arrow" basic content="Price: Ascending"
+                                        onClick={() => orderByPriceAscending()}/>
+                                <Button icon="down arrow" basic content="Price: Descending"
+                                        onClick={() => orderByPriceDescending()}/>
+                                <Dropdown
+                                    placeholder="Category"
+                                    selection
+                                    options={categories}
+                                    defaultValue="all"
+                                    onChange={orderByCategory}
+
+                                    // TODO: Find out how to add onClick functionality to dynamic selection values
+                                ></Dropdown>
+                            </div>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <div className="product-list-item-cards">
+                                <Card.Group itemsPerRow={3}>
+                                    {products["Products"].map((item) => (
+                                        <Card key={item.id}>
+                                            <Image
+                                                src="https://i.ytimg.com/vi/z_wcL_zg2hM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDo4Dzvudez6Op0QRM9DCt9INaCZQ"/>
+                                            <Card.Content>
+                                                <Card.Header content={item.name}/>
+                                                <Card.Meta content={item.category}/>
+                                                <Card.Description content={item.desc}/>
+                                            </Card.Content>
+                                            <Card.Content>
+                                                <div className="item-card-body">
+                                                    <div className="price-n-quantity">
+                                                        <div className="item-card-price">${item.price}</div>
+                                                        <Button.Group basic size="medium">
+                                                            <Button icon="plus"/>
+                                                            <Button disabled content="14"/>
+                                                            <Button icon="minus"/>
+                                                        </Button.Group>
+                                                    </div>
+                                                    <div className="item-card-cart-n-wishlist">
+                                                        <Button icon="heart" primary onClick={() => likeItem(item.id)}/>
+                                                        <Button positive animated="fade" size="medium" fluid>
+                                                            <Button.Content visible>
+                                                                <Icon name="arrow right"/>
+                                                            </Button.Content>
+                                                            <Button.Content hidden>Buy</Button.Content>
+                                                        </Button>
+                                                    </div>
+                                                </div>
+                                            </Card.Content>
+                                        </Card>
+                                    ))}
+                                </Card.Group>
+                            </div>
+                        </Grid.Row>
+                    </Grid>
+                    {/*</React.Suspense>*/}
+                </div>
+            </>
+        );
+    }
 }
 
 export default Products;
