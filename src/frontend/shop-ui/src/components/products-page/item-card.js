21baaf3a2049a564939ef790cc45b658c8a77346
Ivan Jackson <ivan.jackson@upr.edu>
2022-08-10 09:15:06 -0400

    Pulls real data

--- src/frontend/shop-ui/src/components/products-page/item-card.js
+++ src/frontend/shop-ui/src/components/products-page/item-card.js
@@ -1,132 +1,139 @@
-import { Button, Card, Icon, Image, Grid, Dropdown } from "semantic-ui-react";
-import React from "react";
+import {Button, Card, Icon, Image, Grid, Dropdown} from "semantic-ui-react";
+import React, {useEffect} from "react";
 import "./item-card.css";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import Loading from "../utility/loading";
+import {getAllProducts} from "../../features/products/productSlice";
+
 
 function Products() {
-  const {products, isLoading} = useSelector((store) => store.product);
+    const dispatch = useDispatch();
 
-  const categories = [
-    {
-      key: 0,
-      text: "Audio",
-      value: "audio",
-    },
-    {
-      key: 1,
-      text: "Collectibles",
-      value: "collectibles",
-    },
-    {
-      key: 2,
-      text: "Consoles",
-      value: "consoles",
-    },
-    {
-      key: 3,
-      text: "Controllers",
-      value: "controllers",
-    },
-    {
-      key: 4,
-      text: "Gift Cards",
-      value: "gift cards",
-    },
-    {
-      key: 5,
-      text: "PC",
-      value: "pc",
-    },
-    {
-      key: 6,
-      text: "Peripherals",
-      value: "peripherals",
-    },
-    {
-      key: 7,
-      text: "Smartphones",
-      value: "smartphones",
-    },
-    {
-      key: 8,
-      text: "Storage",
-      value: "storage",
-    },
-    {
-      key: 9,
-      text: "Video Games",
-      value: "video games",
-    },
-  ];
+    const {products, isLoading} = useSelector((store) => store.product);
 
-  if (isLoading) {
-    return <Loading/>
-  } else {
-    return (
-      <>
-        <div className="product-list-body">
-          <Grid celled="internally">
-            <Grid.Row>
-              <div className="product-list-sort-selection">
-                <h1>Sort by</h1>
-                {/* TODO: Add indicator of which button was clicked */}
-                {/* TODO: Disable category button if filter by price has been selected and vice versa */}
-                <Button icon="up arrow" basic content="Price: Ascending"/>
-                <Button icon="down arrow" basic content="Price: Descending"/>
-                <Dropdown
-                  placeholder="Category"
-                  selection
-                  options={categories}
+    useEffect(() => {
+        dispatch(getAllProducts())
+    }, [])
+    const categories = [
+        {
+            key: 0,
+            text: "Audio",
+            value: "audio",
+        },
+        {
+            key: 1,
+            text: "Collectibles",
+            value: "collectibles",
+        },
+        {
+            key: 2,
+            text: "Consoles",
+            value: "consoles",
+        },
+        {
+            key: 3,
+            text: "Controllers",
+            value: "controllers",
+        },
+        {
+            key: 4,
+            text: "Gift Cards",
+            value: "gift cards",
+        },
+        {
+            key: 5,
+            text: "PC",
+            value: "pc",
+        },
+        {
+            key: 6,
+            text: "Peripherals",
+            value: "peripherals",
+        },
+        {
+            key: 7,
+            text: "Smartphones",
+            value: "smartphones",
+        },
+        {
+            key: 8,
+            text: "Storage",
+            value: "storage",
+        },
+        {
+            key: 9,
+            text: "Video Games",
+            value: "video games",
+        },
+    ];
 
-                  // TODO: Find out how to add onClick functionality to dynamic selection values
-                ></Dropdown>
-              </div>
-            </Grid.Row>
-            <Grid.Row>
-              <div className="product-list-item-cards">
-                <Card.Group itemsPerRow={3}>
-                  {products["Products"].map((item) => (
-                    <Card key={item.id}>
-                      <Image
-                        src="https://i.ytimg.com/vi/z_wcL_zg2hM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDo4Dzvudez6Op0QRM9DCt9INaCZQ"/>
-                      <Card.Content>
-                        <Card.Header content={item.name}/>
-                        <Card.Meta content={item.category}/>
-                        <Card.Description content={item.desc}/>
-                      </Card.Content>
-                      <Card.Content>
-                        <div className="item-card-body">
-                          <div className="price-n-quantity">
-                            <div className="item-card-price">${item.price}</div>
-                            <Button.Group basic size="medium">
-                              <Button icon="plus"/>
-                              <Button disabled content="14"/>
-                              <Button icon="minus"/>
-                            </Button.Group>
-                          </div>
-                          <div className="item-card-cart-n-wishlist">
-                            <Button icon="heart" primary/>
-                            <Button positive animated="fade" size="medium" fluid>
-                              <Button.Content visible>
-                                <Icon name="arrow right"/>
-                              </Button.Content>
-                              <Button.Content hidden>Add to Cart</Button.Content>
-                            </Button>
-                          </div>
-                        </div>
-                      </Card.Content>
-                    </Card>
-                  ))}
-                </Card.Group>
-              </div>
-            </Grid.Row>
-          </Grid>
-          {/*</React.Suspense>*/}
-        </div>
-      </>
-    );
-  }
+    if (isLoading) {
+        return <Loading/>
+    } else {
+        return (
+            <>
+                <div className="product-list-body">
+                    <Grid celled="internally">
+                        <Grid.Row>
+                            <div className="product-list-sort-selection">
+                                <h1>Sort by</h1>
+                                {/* TODO: Add indicator of which button was clicked */}
+                                {/* TODO: Disable category button if filter by price has been selected and vice versa */}
+                                <Button icon="up arrow" basic content="Price: Ascending"/>
+                                <Button icon="down arrow" basic content="Price: Descending"/>
+                                <Dropdown
+                                    placeholder="Category"
+                                    selection
+                                    options={categories}
+
+                                    // TODO: Find out how to add onClick functionality to dynamic selection values
+                                ></Dropdown>
+                            </div>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <div className="product-list-item-cards">
+                                <Card.Group itemsPerRow={3}>
+                                    {products["Products"].map((item) => (
+                                        <Card key={item.id}>
+                                            <Image
+                                                src="https://i.ytimg.com/vi/z_wcL_zg2hM/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDo4Dzvudez6Op0QRM9DCt9INaCZQ"/>
+                                            <Card.Content>
+                                                <Card.Header content={item.name}/>
+                                                <Card.Meta content={item.category}/>
+                                                <Card.Description content={item.desc}/>
+                                            </Card.Content>
+                                            <Card.Content>
+                                                <div className="item-card-body">
+                                                    <div className="price-n-quantity">
+                                                        <div className="item-card-price">${item.price}</div>
+                                                        <Button.Group basic size="medium">
+                                                            <Button icon="plus"/>
+                                                            <Button disabled content="14"/>
+                                                            <Button icon="minus"/>
+                                                        </Button.Group>
+                                                    </div>
+                                                    <div className="item-card-cart-n-wishlist">
+                                                        <Button icon="heart" primary/>
+                                                        <Button positive animated="fade" size="medium" fluid>
+                                                            <Button.Content visible>
+                                                                <Icon name="arrow right"/>
+                                                            </Button.Content>
+                                                            <Button.Content hidden>Add to Cart</Button.Content>
+                                                        </Button>
+                                                    </div>
+                                                </div>
+                                            </Card.Content>
+                                        </Card>
+                                    ))}
+                                </Card.Group>
+                            </div>
+                        </Grid.Row>
+                    </Grid>
+                    {/*</React.Suspense>*/}
+                </div>
+            </>
+        );
+    }
 }
 
 export default Products;
