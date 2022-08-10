62a449839a09e5044e2cd539873719f044daf378
Ivan Jackson <ivan.jackson@upr.edu>
2022-08-10 10:10:10 -0400

    Implementing fetching for likes

--- src/frontend/shop-ui/src/features/likes/likesSlice.js
+++ src/frontend/shop-ui/src/features/likes/likesSlice.js
@@ -1,54 +1,64 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
 import {likes} from "../../dummy-info/dummy-likes-info";
+import {useSelector} from "react-redux";
+
+//TODO: get userID from the right place
+//
+// const userID = useSelector(store => store.likes)
+const userID = 208;
+const url = 'http://127.0.0.1:5000/goated_the_sql/'.concat(userID).concat('/liked_list')
 
 export const addLikedItem = (id) => {
-  return createAsyncThunk('likes/addLike', () => {
-    return fetch(`http://${id}`);
-  });
+    return createAsyncThunk('likes/addLike', () => {
+        return fetch(`http://${id}`);
+    });
 }
 
 export const getAllLikes = createAsyncThunk('likes/getAllLikes', () => {
-  return fetch('http://');
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) =>
+            console.log(error));
 });
 
 const likesSlice = createSlice({
-  name: "likes",
-  initialState: {
-    items: likes,
-    //It is true because the likes is fetched at the beginning
-    isLoading: false,
-  },
-  reducers: {
-    removeLikedItem(state, action) {
-      const id = action.payload;
-      const item = state.items.find(item => item.id === id);
-      state.items.splice(state.items.indexOf(item), 1);
+    name: "likes",
+    initialState: {
+        items: likes,
+        //It is true because the likes is fetched at the beginning
+        isLoading: false,
     },
-  },
-  extraReducers: {
-    [addLikedItem.pending] : (state) => {
-      state.isLoading = true;
+    reducers: {
+        removeLikedItem(state, action) {
+            const id = action.payload;
+            const item = state.items.find(item => item.id === id);
+            state.items.splice(state.items.indexOf(item), 1);
+        },
     },
-    [addLikedItem.fulfilled] : (state, action) => {
-      state.items.push(action.payload);
-      state.isLoading = false;
-    },
-    [addLikedItem.rejected] : (state) => {
-      state.isLoading = false;
-    },
+    extraReducers: {
+        [addLikedItem.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [addLikedItem.fulfilled]: (state, action) => {
+            state.items.push(action.payload);
+            state.isLoading = false;
+        },
+        [addLikedItem.rejected]: (state) => {
+            state.isLoading = false;
+        },
 
-    [getAllLikes.pending] : (state) => {
-      state.isLoading = true;
-    },
-    [getAllLikes.fulfilled] : (state, action) => {
-      state.items = action.payload;
-      state.isLoading = false;
-    },
-    [getAllLikes.rejected] : (state) => {
-      state.isLoading = false;
+        [getAllLikes.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [getAllLikes.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.isLoading = false;
+        },
+        [getAllLikes.rejected]: (state) => {
+            state.isLoading = false;
+        }
     }
-  }
 });
 export const {removeLikedItem} = likesSlice.actions;
 export default likesSlice.reducer;
