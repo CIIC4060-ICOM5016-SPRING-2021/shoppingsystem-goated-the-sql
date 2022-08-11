42e35f4336248217bb67f28163458aa00b6a5c1e
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-08-10 18:56:00 -0600

    Added id undefined edge case handling to cart-page.js

--- src/frontend/shop-ui/src/routes/cart-page.js
+++ src/frontend/shop-ui/src/routes/cart-page.js
@@ -8,6 +8,7 @@ import "./cart-page.css"
 import {useDispatch, useSelector} from "react-redux";
 import Loading from "../components/utility/loading";
 import {clearCart, getCartItems, setTotal} from "../features/cart/cartSlice";
+import {fetchAccountInfo} from "../features/user/accountSlice";
 
 function CartPage() {
   const {cartItems, isLoading, total} = useSelector(state => state.cart)
@@ -15,7 +16,11 @@ function CartPage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCartItems(id));
+    if (id !== undefined) {
+      dispatch(getCartItems(id));
+    } else {
+      dispatch(fetchAccountInfo(187));
+    }
   }, [dispatch, id]);
 
   function calculatedTotal(cartItems) {
