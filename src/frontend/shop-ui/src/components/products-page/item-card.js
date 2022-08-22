f09ba41aa98f714fd6d6a7c72e2c21e87d8b1863
Francisco Casiano <personal@franciscocasiano.com>
2022-08-22 19:33:14 -0400

    Foundation for product updates has been established. Delete product from store now works

--- src/frontend/shop-ui/src/components/products-page/item-card.js
+++ src/frontend/shop-ui/src/components/products-page/item-card.js
@@ -1,17 +1,24 @@
-import {Button, Card, Dropdown, Grid, Icon, Image} from "semantic-ui-react";
+import {Button, Card, Dropdown, Form, Grid, Icon, Image} from "semantic-ui-react";
 import React, {useEffect} from "react";
 
 import "./item-card.css";
 import Loading from "../utility/loading";
 
 import {useDispatch, useSelector} from "react-redux";
-import {filterByCat, getAllProducts, orderByPriceAsc, orderByPriceDesc} from "../../features/products/productSlice";
+import {
+  deleteProduct,
+  filterByCat,
+  getAllProducts,
+  orderByPriceAsc,
+  orderByPriceDesc, setProductDetails,
+  updateProduct
+} from "../../features/products/productSlice";
 import {addProductToCartDB} from "../../features/cart/cartSlice";
 import {addLikedItemDB} from "../../features/likes/likesSlice";
 
 function Products() {
   const {products, isLoading} = useSelector((store) => store.product);
-  const {id} = useSelector((store) => store.user.details);
+  const {id, admin} = useSelector((store) => store.user.details);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,7 +30,6 @@ function Products() {
       window.location.href = "/";
     }
   }, [dispatch, products, id]);
-
   const categories = [
     {
       key: 0,
@@ -85,85 +91,123 @@ function Products() {
   function likeItem(itemID) {
     dispatch(addLikedItemDB({user_id: id, product: itemID}));
   }
+  function addToCart(item) {
+    dispatch(addProductToCartDB({product: item, user_id: id}));
+  }
 
   function orderByPriceAscending() {
     dispatch(orderByPriceAsc());
   }
-
   function orderByPriceDescending() {
     dispatch(orderByPriceDesc());
   }
-
   function orderByCategory(e, category) {
     dispatch(filterByCat(category));
   }
 
-  function addToCart(item) {
-    dispatch(addProductToCartDB({product: item, user_id: id}));
+  function removeProduct(item) {
+    dispatch(deleteProduct({requesterId: id, product: item})).then(() => {dispatch(getAllProducts())});
+  }
+
+  function showAdminDetails(item) {
+    if (admin === true) {
+      return (
+          <>
+            <Image
+                src="https://i.ytimg.com/vi/hYIsp4qA-z4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDkz9Org_0z_DjBie5FR9YuflgXcg"/>
+            <Card.Content>
+              <Form size="small">
+                <Form.Input label="Product" placeholder={item.name}/>
+                <Form.Input label="Category" placeholder={item.category}/>
+                <Form.TextArea label="Description" placeholder={item.desc}/>
+                <Form.Input label="Price" placeholder={item.price}/>
+                <Form.Input label="Stock" placeholder={item.stock}/>
+              </Form>
+            </Card.Content>
+            <Card.Content>
+              <div className="item-card-body">
+                <div className="item-card-cart-n-wishlist">
+                  <Button icon="trash" content="Remove" negative fluid onClick={() => removeProduct(item)}/>
+                </div>
+              </div>
+            </Card.Content>
+          </>
+      );
+    } else {
+      return (
+          <>
+            <Image
+                src="https://i.ytimg.com/vi/hYIsp4qA-z4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDkz9Org_0z_DjBie5FR9YuflgXcg"/>
+            <Card.Content>
+              <Card.Header content={item.name}/>
+              <Card.Meta content={item.category}/>
+              <Card.Description content={item.desc}/>
+            </Card.Content>
+            <Card.Content>
+              <div className="item-card-body">
+                <div className="price-n-quantity">
+                  <div className="item-card-price">${item.price}</div>
+                  <h4>Available: ${item.stock}</h4>
+                </div>
+                <div className="item-card-cart-n-wishlist">
+                  <Button icon="heart" primary onClick={() => likeItem(item)}/>
+                  <Button positive animated="fade" size="medium" fluid
+                          onClick={() => addToCart(item)}>
+                    <Button.Content visible>
+                      <Icon name="arrow right"/>
+                    </Button.Content>
+                    <Button.Content hidden>Add to Cart</Button.Content>
+                  </Button>
+                </div>
+              </div>
+            </Card.Content>
+          </>
+      );
+    }
   }
 
   if (isLoading) {
     return <Loading/>
   } else {
     return (
-      <>
-        <div className="product-list-body">
-          <Grid celled="internally">
-            <Grid.Row>
-              <div className="product-list-sort-selection">
-                <h1>Sort by</h1>
-                {/* TODO: Add indicator of which button was clicked */}
-                {/* TODO: Disable category button if filter by price has been selected and vice versa */}
-                <Button icon="up arrow" basic content="Price: Ascending" onClick={() => orderByPriceAscending()}/>
-                <Button icon="down arrow" basic content="Price: Descending" onClick={() => orderByPriceDescending()}/>
-                <Dropdown
-                  placeholder="Category"
-                  selection
-                  options={categories}
-                  defaultValue="all"
-                  onChange={orderByCategory}
+        <>
+          <div className="product-list-body">
+            <Grid celled="internally">
+              <Grid.Row>
+                <div className="product-list-sort-selection">
+                  <h1>Sort by</h1>
+                  {/* TODO: Add indicator of which button was clicked */}
+                  {/* TODO: Disable category button if filter by price has been selected and vice versa */}
+                  <Button icon="up arrow" basic content="Price: Ascending"
+                          onClick={() => orderByPriceAscending()}/>
+                  <Button icon="down arrow" basic content="Price: Descending"
+                          onClick={() => orderByPriceDescending()}/>
+                  <Dropdown
+                      placeholder="Category"
+                      selection
+                      options={categories}
+                      defaultValue="all"
+                      onChange={orderByCategory}
 
-                  // TODO: Find out how to add onClick functionality to dynamic selection values
-                ></Dropdown>
-              </div>
-            </Grid.Row>
-            <Grid.Row>
-              <div className="product-list-item-cards">
-                <Card.Group itemsPerRow={3}>
-                  {products["Products"].map((item) => (
-                    <Card key={item.id}>
-                      <Image
-                        src="https://i.ytimg.com/vi/hYIsp4qA-z4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDkz9Org_0z_DjBie5FR9YuflgXcg"/>
-                      <Card.Content>
-                        <Card.Header content={item.name}/>
-                        <Card.Meta content={item.category}/>
-                        <Card.Description content={item.desc}/>
-                      </Card.Content>
-                      <Card.Content>
-                        <div className="item-card-body">
-                          <div className="price-n-quantity">
-                            <div className="item-card-price">${item.price}</div>
-                          </div>
-                          <div className="item-card-cart-n-wishlist">
-                            <Button icon="heart" primary onClick={() => likeItem(item)}/>
-                            <Button positive animated="fade" size="medium" fluid onClick={() => addToCart(item)}>
-                              <Button.Content visible>
-                                <Icon name="arrow right"/>
-                              </Button.Content>
-                              <Button.Content hidden>Add to Cart</Button.Content>
-                            </Button>
-                          </div>
-                        </div>
-                      </Card.Content>
-                    </Card>
-                  ))}
-                </Card.Group>
-              </div>
-            </Grid.Row>
-          </Grid>
-          {/*</React.Suspense>*/}
-        </div>
-      </>
+                      // TODO: Find out how to add onClick functionality to dynamic selection values
+                  ></Dropdown>
+                </div>
+              </Grid.Row>
+              <Grid.Row>
+                <div className="product-list-item-cards">
+                  <Card.Group itemsPerRow={3}>
+                    {products["Products"].map((item) => (
+                        <Card key={item.id}>
+                          {showAdminDetails(item)}
+                        </Card>
+                    ))}
+                  </Card.Group>
+                </div>
+              </Grid.Row>
+            </Grid>
+            {/*</React.Suspense>*/}
+          </div>
+        </>
     );
   }
 }
--- src/frontend/shop-ui/src/features/products/productSlice.js
+++ src/frontend/shop-ui/src/features/products/productSlice.js
@@ -2,9 +2,51 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
 export const getAllProducts = createAsyncThunk('products/getAllProducts', () => {
   return fetch('http://127.0.0.1:5000/goated_the_sql/products/all')
-    .then((response) => response.json())
-    .catch((error) =>
-      console.log(error));
+      .then((response) => response.json())
+      .catch((error) =>
+          console.log(error));
+});
+export const updateProduct = createAsyncThunk('products/updateProduct', (requiredInfo) => {
+  const {requesterId, product} = requiredInfo;
+
+  const productBodyRequest = {
+    product_id: product.id,
+    name: product.name,
+    description: product.desc,
+    price: product.price,
+    category: product.category,
+    stock: product.stock,
+    visible: product.visible
+  }
+
+  const requestOptions = {
+    method: 'PUT',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify([
+          {
+            user_id: requesterId,
+          },
+          productBodyRequest
+        ]
+    )
+  }
+
+  return fetch(`http://127.0.0.1:5000/goated_the_sql/product/${product.id}`, requestOptions)
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
+});
+export const deleteProduct = createAsyncThunk('products/deleteProduct', (requiredInfo) => {
+  const {requesterId, product} = requiredInfo;
+
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({user_id: requesterId})
+  }
+
+  return fetch(`http://127.0.0.1:5000/goated_the_sql/product/${product.id}`, requestOptions)
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
 });
 
 export const productSlice = createSlice({
@@ -17,6 +59,16 @@ export const productSlice = createSlice({
     modified: false,
   },
   reducers: {
+    setProductDetails: (state, action) => {
+      //find the product with the same id as the one we want to update and update it
+      const product = state.products["Products"].find((product) => product.id === action.payload.id);
+      product.name = action.payload.name;
+      product.desc = action.payload.desc;
+      product.price = action.payload.price;
+      product.category = action.payload.category;
+      product.stock = action.payload.stock;
+      product.visible = action.payload.visible;
+    },
     orderByPriceAsc: (state) => {
       state.products["Products"].sort((a, b) => a.price - b.price);
     },
@@ -52,9 +104,21 @@ export const productSlice = createSlice({
     [getAllProducts.rejected]: (state) => {
       console.log("Products loading failed");
       state.isLoading = false;
+    },
+    [updateProduct.pending]: (state) => {
+      console.log("Updating product");
+      state.isLoading = true;
+    },
+    [updateProduct.fulfilled]: (state) => {
+      console.log("Product updated");
+      state.isLoading = false;
+    },
+    [updateProduct.rejected]: (state) => {
+      console.log("Product update failed");
+      state.isLoading = false;
     }
   }
 });
 
-export const {orderByPriceAsc, orderByPriceDesc, filterByCat} = productSlice.actions;
+export const {orderByPriceAsc, orderByPriceDesc, filterByCat, setProductDetails} = productSlice.actions;
 export default productSlice.reducer;
